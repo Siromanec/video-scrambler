4eb25dbc46bd33fa02b265986e287f3e9e80a54a
Siromanec <serhii.ivanov@ucu.edu.ua>
2025-04-06 14:24:49 +0300

    added data valid flag

--- FPGA/TVP5147M1_decoder_interface/src/video/cryptography/line_rotation/scrambling/rtl/line_rotator.v
+++ FPGA/TVP5147M1_decoder_interface/src/video/cryptography/line_rotation/scrambling/rtl/line_rotator.v
@@ -6,7 +6,8 @@ module line_rotator #(parameter MODE = 0)(
     input wire [7:0] raw_cut_position,
     input wire V,
     input wire H,
-    output reg [9:0] data_out
+    output reg [9:0] data_out,
+    output reg data_valid
 );
     localparam ACTIVE_LINE_SIZE = 2 * 720;
     localparam MAX_BUFFER_SIZE = 2048;
@@ -44,14 +45,16 @@ module line_rotator #(parameter MODE = 0)(
 				get_read_idx = write_index;
 		end
 	 endfunction
-	 
-
+	 localparam GARBAGE_LINES = 2;
+    reg [1:0] line_switch_count;
     always @(posedge clk or negedge reset_n) begin
         if (!reset_n) begin
 
             switch_buffer <= 0;
             data_out <= 0;
             prev_H <= H;
+            data_valid <= 0;
+            line_switch_count <=0;
 
 				if (!H)
 					write_index <= 0;
@@ -67,6 +70,11 @@ module line_rotator #(parameter MODE = 0)(
 						line_buffer[!switch_buffer][get_read_idx(0, cut_position, H, V)] <= data_in;
 						data_out <= line_buffer[switch_buffer][0];
 					end
+
+					if (line_switch_count < GARBAGE_LINES)
+					   line_switch_count <= line_switch_count + 1;
+					else
+					   data_valid <= 0;
 
 					switch_buffer <= !switch_buffer;
 					write_index <= 1;
