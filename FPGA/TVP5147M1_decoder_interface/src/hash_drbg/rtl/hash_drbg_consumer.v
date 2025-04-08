08563f515ef96fae98d4d06a66e58f8d487fc097
Siromanec <serhii.ivanov@ucu.edu.ua>
2025-04-08 14:52:13 +0300

    wrote and tested hash drbg consumer; needs randomness test just to be sure; works independently of sample count in a line and does not depend on number of lines;

---
+++ FPGA/TVP5147M1_decoder_interface/src/hash_drbg/rtl/hash_drbg_consumer.v
@@ -0,0 +1,146 @@
+module hash_drbg_consumer #(parameter DATA_WIDTH_IN = 256,
+                            parameter DATA_WIDTH_OUT = 8)
+                           (input wire H,
+                            input wire V,
+                            input wire clk,
+                            input wire reset_n,
+                            input wire [DATA_WIDTH_IN-1:0] data_in,
+                            input wire data_in_valid,
+                            input wire generator_busy,
+                            output reg [DATA_WIDTH_OUT-1:0] data_out,
+                            output reg data_out_valid,
+                            output reg need_next
+                            );
+
+   localparam BUFFER_SIZE = DATA_WIDTH_IN/DATA_WIDTH_OUT;
+   localparam BUFFER_ADDRESS_BITS = $clog2(BUFFER_SIZE);
+   reg [DATA_WIDTH_OUT-1:0] data_buffer [0:1][0:BUFFER_SIZE - 1];
+   reg current_write_buffer;
+
+   reg [BUFFER_ADDRESS_BITS-1:0] wa;
+   reg [BUFFER_ADDRESS_BITS-1:0] ra;
+   localparam GET_NEW_DATA_IDLE = 0,
+              GET_NEW_DATA_NEXT = 1,
+              GET_NEW_DATA_WAIT = 2,
+              GET_NEW_DATA_FILL = 3;
+   reg [$clog2(4) - 1:0] get_new_data_state;
+
+   reg first_read_iteration;
+   reg first_write_iteration;
+
+   reg read_done;
+   reg write_done;
+
+   reg do_read;
+   reg do_write;
+	/*
+	-----------------------------------
+		WRITE CONTROL
+	-----------------------------------
+	*/
+   always @(posedge clk or negedge reset_n) begin : get_new_data
+      if (!reset_n) begin
+         // DOES NOT MODIFY DO_READ
+         // MODIFIES WRITE_DONE
+         get_new_data_state <= GET_NEW_DATA_IDLE;
+         need_next <= 0;
+         wa <= 0;
+         write_done <= 0;
+      end else begin
+         case (get_new_data_state)
+            GET_NEW_DATA_IDLE: begin
+               if(do_write && !generator_busy) begin
+                  write_done <= 0;
+                  need_next <= 1;
+                  get_new_data_state <= GET_NEW_DATA_WAIT;
+               end
+            end
+            GET_NEW_DATA_WAIT: begin
+               need_next <= 0;
+               if (data_in_valid)
+                  get_new_data_state <= GET_NEW_DATA_FILL;
+            end
+            GET_NEW_DATA_FILL: begin
+               data_buffer[current_write_buffer][wa] <= data_in[wa * DATA_WIDTH_OUT +: DATA_WIDTH_OUT];
+               if (wa != BUFFER_SIZE - 1)
+                  wa <= wa + 1;
+               else begin
+                  get_new_data_state <= GET_NEW_DATA_IDLE;
+                  wa <= 0;
+                  write_done <= 1;
+               end
+            end
+         endcase
+      end
+   end
+	/*
+	-----------------------------------
+		READ CONTROL
+	-----------------------------------
+	*/
+   always @(posedge H or negedge reset_n) begin
+      if (!reset_n) begin
+         ra <= 0;
+         data_out_valid <= 0;
+         read_done <= 0;
+      end else if (!V && do_read) begin
+         data_out_valid <= 1;
+         data_out <= data_buffer[!current_write_buffer][ra];
+         if (ra != BUFFER_SIZE - 1) begin
+            ra <= ra + 1;
+            read_done <= 0;
+         end else begin
+            ra <= 0;
+            read_done <= 1;
+         end
+      end else begin
+         read_done <= 0;
+      end
+   end
+
+	
+	/*
+	-----------------------------------
+		LOGIC CONTROL
+	-----------------------------------
+	*/
+	reg prev_write_done, prev_read_done, prev_V, prev_H;
+	wire write_done_rise =  write_done && !prev_write_done;
+	wire read_done_rise  =  read_done  && !prev_read_done;
+	wire V_rise = V && !prev_V;
+	wire V_fall = !V && prev_V;
+	wire H_rise = H && !prev_H;
+	always @(posedge clk or negedge reset_n) begin
+		if (!reset_n) begin
+			prev_write_done <= 0;
+			prev_read_done <= 0;
+
+			first_write_iteration <= 1;
+			do_read <= 0;
+			do_write <= 1;
+			current_write_buffer <= 0;
+		end else begin
+			prev_write_done <= write_done;
+			prev_read_done <= read_done;
+			prev_V <= V;
+			prev_H <= H;
+			if (write_done_rise || read_done_rise || first_write_iteration || H_rise || V_fall) begin
+				if (write_done && (read_done || first_write_iteration)) begin
+
+					first_write_iteration <= 0;
+					do_read <= 1;
+					if (H_rise || V_fall) begin
+				      current_write_buffer <= ~current_write_buffer;
+					   do_write <= 1;
+					end
+				end else begin
+					if (read_done)
+						do_read <= 0;
+					if (write_done)
+						do_write <= 0;
+				end
+			end
+		end
+   end
+
+endmodule
---
+++ FPGA/TVP5147M1_decoder_interface/src/hash_drbg/tb/hash_drbg_consumer_tb.v
@@ -0,0 +1,159 @@
+`timescale 10ns / 1ns
+
+module hash_drbg_consumer_tb;
+
+
+   localparam VIDEO_FILE_LOCATION = "video_f60.bin";
+   localparam LINE_SIZE = 2 * 858;
+
+   localparam LINE_COUNT = 525;
+//   localparam TOTAL_FRAMES = 60;
+   localparam TOTAL_FRAMES = 10;
+   localparam TOTAL_LINES = LINE_COUNT * TOTAL_FRAMES;
+   localparam TOTAL_BYTES = LINE_SIZE * TOTAL_LINES;
+
+
+   reg clk_sig;
+   reg reset_n_sig;
+   reg reset_n_drbg_sig;
+   reg [9:0] bt_656_sig;
+   wire H_sig;
+   wire V_sig;
+   wire F_sig;
+   reg [7:0] cut_position;
+
+   reg next_seed;
+   wire next_bits;
+   reg init;
+   reg [255:0] entropy;
+   wire init_ready;
+   wire next_bits_ready;
+   wire [255:0] random_bits;
+   wire [7:0] random_bits_serial;
+   wire random_bits_serial_valid;
+   wire [63:0] reseed_counter;
+   wire generator_busy;
+
+   double_hash_drbg double_hash_drbg_0 (
+       .is_master_mode(0),
+       .reset_n(reset_n_drbg_sig),
+       .clk(clk_sig),
+       .next_seed(V_sig),
+       .next_bits(next_bits),
+       .init(init),
+       .entropy(entropy),
+       .init_ready(init_ready),
+       .next_bits_ready(next_bits_ready),
+       .random_bits(random_bits),
+       .reseed_counter(reseed_counter),
+       .busy(generator_busy)
+       );
+//    defparam double_hash_drbg_0.BITS_GENERATOR_MAX_CYCLE = BITS_GENERATOR_MAX_CYCLE; // irrelevant
+    ///defparam double_hash_drbg_0.SEED_GENERATOR_MAX_CYCLE = 2;
+
+   sync_parser sync_parser_inst (
+      .clk(clk_sig),
+      .reset_n(reset_n_sig),
+      .bt_656 (bt_656_sig),
+      .H(H_sig),
+      .V(V_sig),
+      .F(F_sig)
+   );
+
+   hash_drbg_consumer hash_drbg_consumer_inst
+   (
+      .H(H_sig) ,	// input  H_sig
+      .V(V_sig) ,	// input  V_sig
+      .clk(clk_sig) ,	// input  clk_sig
+      .reset_n(reset_n_sig) ,	// input  reset_n_sig
+      .data_in(random_bits) ,	// input [(DATA_WIDTH_IN-1):0] data_in_sig
+      .data_in_valid(next_bits_ready) ,	// input  data_in_valid_sig
+      .generator_busy(generator_busy),
+      .data_out(random_bits_serial) ,	// output [(DATA_WIDTH_OUT-1):0] data_out_sig
+      .data_out_valid(random_bits_serial_valid) ,	// output  data_out_valid_sig
+      .need_next(next_bits) 	// output  need_next_sig
+   );
+
+   defparam hash_drbg_consumer_inst.DATA_WIDTH_IN = 256;
+   defparam hash_drbg_consumer_inst.DATA_WIDTH_OUT = 8;
+
+//   reg [7:0] video_data [0:TOTAL_LINES-1];
+   reg [7:0] video_value;
+   integer fd;
+   integer fd_out;
+
+   time i;
+   time j;
+   reg [31:0] seed;
+
+   reg [7:0] line_store [0:(LINE_SIZE - 1)];
+   initial begin
+//      cut_position = $random(seed) % 256;
+      fd = $fopen(VIDEO_FILE_LOCATION, "rb");
+      if (fd == 0) begin
+         $display("Error: Could not open file %s", VIDEO_FILE_LOCATION);
+         $display("fd = %d", fd);
+         $finish;
+      end
+
+      clk_sig = 0;
+      reset_n_sig = 0;
+      reset_n_drbg_sig = 0;
+      bt_656_sig = 0;
+
+       init = 1'b0;
+       next_seed = 1'b0;
+       entropy = 256'h0;
+
+
+      #1;
+      clk_sig = 0;
+      #1;
+      clk_sig = 1;
+      reset_n_drbg_sig = 1;
+      init = 1'b1;
+      while (!init_ready) begin
+         #1;
+         clk_sig = 0;
+         #1;
+         clk_sig = 1;
+      end
+      init = 1'b0;
+      $display("\nInit ready");
+      reset_n_sig = 1;
+
+
+
+
+
+      for (i = 0; i < TOTAL_BYTES / LINE_SIZE; i = i + 1) begin
+         for (j= 0; j < LINE_SIZE; j = j + 1) begin
+            $fgets(video_value, fd);
+            line_store[j] = video_value;
+         end
+/*         if (!V_sig) begin // potential bug if it is vsync at the beginning of read line
+            cut_position = {$random(seed)} % 256;
+         end*/
+         cut_position = random_bits_serial;
+
+         $display("cut_position: %d", cut_position);
+         for (j= 0; j < LINE_SIZE; j = j + 1) begin
+
+//            $display("cut_position: %d", cut_position);
+            video_value = line_store[j];
+
+            bt_656_sig = {video_value, 2'b00};
+
+            #1;
+            clk_sig = 0;
+            #1;
+            clk_sig = 1;
+         end
+
+      end
+      $fclose(fd);
+      $stop;
+
+   end
+
+endmodule
